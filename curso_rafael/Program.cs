62d2d344cc52118a1ae80142d97915107a93a618
omagister <jrenatodias@gmail.com>
2022-04-03 16:01:56 -0300

    primeiro commit do rafael

---
+++ curso_rafael/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace curso_rafael
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int primeiroNumero;
+           primeiroNumero = 27;
+           Console.WriteLine($"primeiroNumeo: {primeiroNumero}");
+           int segundoNumero = 12;
+           Console.WriteLine($"segundoNumero: {segundoNumero}");
+           Console.WriteLine($"a soma dos dois numeros é: {primeiroNumero+segundoNumero}");        
+           Console.WriteLine($"a subtração de primeiroNumero pelo segundoNumero e igual a: {primeiroNumero-segundoNumero}");
+           Console.WriteLine($"a multiplicação de segundoNumero por três e igual a: {segundoNumero*3}");
+           Console.WriteLine($"a divisão de primeiroNumero por 3 e igual a: {primeiroNumero/3}");
+           
+         }
+    }
+}
